f9a4225cc10e479c91554e3a85afd65722894b71
Duy Thanh <lothanhduy2003@gmail.com>
2023-03-09 21:13:03 +0700

    Tạo activity update password

---
+++ app/src/main/java/com/example/ad/screens/activity_screens/UpdatePasswordActivity.java
@@ -0,0 +1,16 @@
+package com.example.ad.screens.activity_screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ad.R;
+
+public class UpdatePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_password);
+    }
+}
